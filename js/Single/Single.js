e8b749a84137b68b0d80b8875f1e199046704a74
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2021-12-12 11:24:02 -0500

    sort method

--- js/Single/Single.js
+++ js/Single/Single.js
@@ -1,3 +1,6 @@
+import { Form } from "../Form/Form.js";
+import { Lightbox } from "../Lightbox/Lightbox.js";
+
 class Single {
   constructor() {
     this.params = new URLSearchParams(window.location.search);
@@ -5,7 +8,7 @@ class Single {
     this.photographerApi = new PhotographerApi("../data.json");
     this.displayPhotographer = document.querySelector(".hero__wrappe");
     this.displayMedias = document.querySelector(".galery__images--wrappe");
-    this.sortMedias = document.querySelector("#sort");
+    this.sortSelect = document.querySelector("#sort");
   }
 
   async getPhotographerById() {
@@ -18,53 +21,103 @@ class Single {
         const { name, portrait, city, country, tagline, tags } = photographer;
         //console.log(photographer);
         this.displayPhotographer.innerHTML = `
-			<div class="hero__wrappe--information">
-					<h1> ${name}</h1>
-					<address>${city} ${country}</address>
-					<p>${tagline}</p>
-					<ul>
-					${tags.map((tag) => `<li class="tag">#${tag}<li/>`)}
-					</ul>
-				</div>
-				<div class="hero__wrappe--btn">
-					<button class="btn open-modal-form">Contactez-nous</button>
-				</div>
-				<div class="hero__wrappe--img">
-					<img src="../images/photographers/${portrait}" alt="${name}">
-				</div> 
-			</div>
+        <div class="hero__wrappe--information">
+            <h1> ${name}</h1>
+            <address>${city} ${country}</address>
+            <p>${tagline}</p>
+            <ul>
+            ${tags.map((tag) => `<li class="tag">#${tag}<li/>`)}
+            </ul>
+          </div>
+          <div class="hero__wrappe--btn">
+            <button class="btn">Contactez-nous</button>
+          </div>
+          <div class="hero__wrappe--img">
+            <img src="../images/photographers/${portrait}" alt="${name}">
+          </div> 
+        </div>
+        
 			`;
+        this.displayInfosPhotographer(photographer);
       }
+
+      return photographer;
     });
+    // init contact form
+    const form = new Form();
+    form.init();
   }
 
   async getMedias() {
     const Api = await this.photographerApi.getPhotographer();
     const medias = Api.media;
+    const mediasByPhotographer = [];
     medias.filter((media) => {
       if (this.id == media.photographerId) {
         let Card = new MediaCard(media);
         this.displayMedias.appendChild(Card.createCard());
+        mediasByPhotographer.push(media);
       }
+
+      this.sortSelect.addEventListener("change", () => {
+        this.sortMediasMethod(mediasByPhotographer);
+      });
+    });
+
+    const lightbox = new Lightbox();
+    lightbox.display();
+  }
+
+  /**
+   * sort array and display new array sorted
+   * @param {Array} mediasToSort
+   */
+  sortMediasMethod(mediasToSort) {
+    let selectOption = this.sortSelect.value;
+    if (selectOption === "popularity") {
+      //console.log("popularity");
+      mediasToSort.sort((a, b) => {
+        return b.likes - a.likes;
+      });
+    } else if (selectOption === "date") {
+      //console.log("Date");
+      mediasToSort.sort((a, b) => {
+        return new Date(a.date) - new Date(b.date);
+      });
+    } else if (selectOption === "title") {
+      //console.log("Ttile");
+      mediasToSort.sort((a, b) => {
+        return a.title.localeCompare(b.title);
+      });
+    }
+    this.displayMedias.innerHTML = "";
+    mediasToSort.map((media) => {
+      let Card = new MediaCard(media);
+      this.displayMedias.appendChild(Card.createCard());
     });
   }
 
-  sortMediasMethod(medias) {
-    this.sortMedias.addEventListener("click", () => {
-      let choice = this.sortMedias.value;
-      if (choice === "popularity") {
-        //console.log("popularity");
-        medias.media.sort((a, b) => {
-          const sortPopularity = b.likes - a.likes;
-          console.log(sortPopularity);
-          return sortPopularity;
-        });
-      } else if (choice === "date") {
-        console.log("date");
-      } else if (choice === "title") {
-        console.log("title");
-      }
-    });
+  /**
+   *
+   * @param {Array} mediasByPhotographer
+   */
+
+  displayInfosPhotographer(photographer) {
+    let price = photographer.price;
+
+    const element = document.createElement("div");
+    element.classList.add("fixed-card");
+    element.innerHTML = `<div class="fixed-card-wrappe">
+                <div> 
+                    <span class="likes">81</span>
+                    <i class="fas fa-heart"></i>
+                </div>
+                <div>
+                    <span> ${price}€/jour</span>
+                </div>
+            </div>`;
+
+    document.body.appendChild(element);
   }
 }
 
