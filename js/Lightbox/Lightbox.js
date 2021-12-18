1092c20edc5b6ceabe223a3a1eb396d0d5ecc9dc
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2021-12-18 11:48:35 -0500

    Ajout des méthodes de fermeture au click et au clavier escape - reste à implementer les méthodes pour récupérer le current image et afficher la suivante et la précédente

--- js/Lightbox/Lightbox.js
+++ js/Lightbox/Lightbox.js
@@ -1,18 +1,108 @@
-class Lightbox {
-  constructor() {
-    this.card = document.querySelectorAll("article");
+export class Lightbox {
+  /**
+   *
+   * @param {Array} mediasPhotographer // Tableau des média d'un photographe
+   */
+  constructor(mediasPhotographer) {
+    this.mediasPhotographer = mediasPhotographer;
+    console.log(this.mediasPhotographer);
   }
 
   display() {
-    for (let i = 0; i < this.card.length; i++) {
-      this.card[i].addEventListener("click", () => {
-        console.log("click");
+    const cards = document.querySelectorAll(".card-img, .card-video");
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].addEventListener("click", () => {
+        // console.log(cards[i]);
+        //let imgSRC = cards[i].getAttribute("src");
+        /*  let index = 1;
+        if (this.mediasPhotographer.findIndex() == index) {
+          console.log("True");
+        } else {
+          console.log("False");
+        } */
+        // console.log(imgSRC);
+        this.displayContentLightbox(this.mediasPhotographer);
+
+        this._left();
+        this._right();
+        this._closeLightbox();
       });
     }
   }
-}
 
-document.addEventListener("DOMContentLoaded", () => {
-  const box = new Lightbox();
-  box.display();
-});
+  /**
+   *
+   * @param {Array} mediasPhotographer
+   */
+
+  currentMedia(mediasPhotographer) {
+    // obtenir l'index du media
+    // le comparer au tableau des medias
+    // Afficher les informations liées à ce média
+  }
+
+  /**
+   *
+   * @param {Object} media
+   */
+  displayContentLightbox(media) {
+    const lightbox = document.querySelector(".lightbox__container ");
+    const contentLightbox = document.querySelector(
+      ".lightbox__container--media"
+    );
+    lightbox.style.display = "flex";
+    contentLightbox.innerHTML = `
+      <img src="../images/${media.image}" alt="">
+      <div class="title">
+          <p>${media.title}</p>
+      </div>`;
+  }
+
+  _right() {
+    const next = document.querySelector(".fa-chevron-right");
+    next.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.nextMethod();
+    });
+
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        this.nextMethod();
+      }
+    });
+  }
+
+  _left() {
+    const prev = document.querySelector(".fa-chevron-left");
+    prev.addEventListener("click", (e) => {
+      this.prevMethod();
+    });
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowLeft") {
+        this.prevMethod();
+      }
+    });
+  }
+
+  _closeLightbox() {
+    const lightbox = document.querySelector(".lightbox__container ");
+    const close = document.querySelector(".close");
+    close.addEventListener("click", () => {
+      lightbox.style.display = "none";
+    });
+
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        lightbox.style.display = "none";
+      }
+    });
+  }
+
+  nextMethod() {
+    console.log("Rigth");
+  }
+
+  prevMethod() {
+    console.log("Left");
+  }
+}
