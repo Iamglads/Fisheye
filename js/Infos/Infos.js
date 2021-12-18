f404565d49860ff6aed73bf91761a177f1eb9630
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2021-12-18 11:41:44 -0500

    Affiche les likes et prix en bas de page

---
+++ js/Infos/Infos.js
@@ -0,0 +1,27 @@
+export class Infos {
+  /**
+   *
+   * @param {Object} infoPhotographer
+   * @param {Number} totalLikes
+   */
+  constructor(infoPhotographer, totalLikes) {
+    this.price = infoPhotographer.price;
+    this.totalLikes = totalLikes;
+  }
+
+  display() {
+    const element = document.createElement("div");
+    element.classList.add("fixed-card");
+    element.innerHTML = `<div class="fixed-card-wrappe">
+                <div> 
+                    <span class="likes">${this.totalLikes}</span>
+                    <i class="fas fa-heart"></i>
+                </div>
+                <div>
+                    <span> ${this.price}€/jour</span>
+                </div>
+            </div>`;
+
+    document.body.appendChild(element);
+  }
+}
